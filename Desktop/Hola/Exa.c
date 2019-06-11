9dbd28f2c937bf091a18dfd38806e84457243404
Emi <carlosemiliano_saucedo@ucol.mx>
2019-06-11 13:37:33 -0500

    Agregue contador timer0

---
+++ Desktop/Hola/Exa.c
@@ -0,0 +1,23 @@
+#include <18f4620.h>
+#include <Exa.h>
+#fuses XT, NOMCLR
+#use delay(clock=40M)
+int16 contador=0;
+#int_timer0
+void timer0()
+{
+    contador++;
+    SET_TIMER0(3036);
+}
+void main()
+{
+setup_timer_0(RTCC_INTERNAL | RTCC_DIV_16); 
+SET_TIMER0(3036);
+enable_interrupts(INT_TIMER0);
+enable_interrupts(GLOBAL);
+    while(true)
+    {
+        
+    }
+    
+}
